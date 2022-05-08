8552de31cc33241846c4f49b4df54aed243e05a8
Xoxko <hohko@outlook.com>
2022-05-08 15:18:43 +0300

    Restart

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"goproject/src"
-)
+
 
 func main() {
-	chip := src.InitCPU("PONG")
-	fmt.Println(chip.OpcodeRun())
-	for i := 0; i < 140; i++ {
-		s := chip.OpcodeText()
-		if s != "1" {
-			fmt.Println(s)
-		}
-	}
+	
 }
